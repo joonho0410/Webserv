ba3c28e7e99cd5f4da10c6d350f73bff5e79662d
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2023-04-21 17:23:18 +0900

    CGI HANDLER -X HEADER

--- CGI/CgiHandler.cpp
+++ CGI/CgiHandler.cpp
@@ -45,6 +45,7 @@ void    CgiHandler::_M_initEnv(Request &request )
     }
     _M_findAndInit("AUTH_TYPE", "AUTHORIZATION", header);
     _M_findAndInit("CONTENT_TYPE", "CONTENT-TYPE", header);
+    _M_findAndInit("HTTP_X_SECRET_HEADER_FOR_TEST", "X-SECRET-HEADER-FOR-TEST", header);
     this->_m_env["CONTENT_LENGTH"] = std::to_string(request.getBody().length()); 
     this->_m_env["GATEWAY_INTERFACE"] = "CGI/1.1";
     this->_m_env["PATH_INFO"] = request.getUrl();
@@ -59,7 +60,6 @@ void    CgiHandler::_M_initEnv(Request &request )
     this->_m_env["SCRIPT_NAME"] = request.getServerUrl();
     this->_m_env["SERVER_PROTOCOL"] = "HTTP/1.1";
     this->_m_env["SERVER_SOFTWARE"] = "webserv/1.0";
-    this->_m_env["HTTP_X_SECRET_HEADER_FOR_TEST"] = "1";
     
     std::cout << "init Env is DONE " << std::endl;
 }
